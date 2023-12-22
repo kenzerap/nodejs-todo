ef40625b5d1886d31aeae28969ec8246596bfc8f
Hoang Nguyen Quoc <hoang.nguyenquoc@positivethinking.tech>
2023-12-22 09:35:35 +0700

    revert cors

--- app.js
+++ app.js
@@ -15,16 +15,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
-  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
